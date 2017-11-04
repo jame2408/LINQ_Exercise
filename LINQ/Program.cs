575ac3d7e434e55cd86242ee2e92626c4dee42d6
jame2408 <jame2408@gmail.com>
2017-11-04 09:51:58 +0800

    沒有 LINQ 的時代

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -10,6 +10,25 @@ namespace LINQ
     {
         static void Main(string[] args)
         {
+            int[] data = { 1, 3, 5, 7, 9};
+            foreach (var item in Filter(data, (s) => s > 5))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        private static List<T> Filter<T>(T[] source, Func<T, bool> func)
+        {
+            List<T> result = new List<T>();
+            foreach (T item in source)
+            {
+                if (func(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
         }
     }
 }
