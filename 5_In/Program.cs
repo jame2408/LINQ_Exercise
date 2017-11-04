7f3f291d022993c34a7d6d5ff5edb5713f143680
jame2408 <jame2408@gmail.com>
2017-11-04 11:26:57 +0800

    LINQ 中做到類似 SQL 語法中的 In。

---
+++ 5_In/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_In
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = {"aaa", "bbb", "ccc"};
+            string[] infilter = {"bbb", "ccc"};
+
+            var result = data.Where(infilter.Contains);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
