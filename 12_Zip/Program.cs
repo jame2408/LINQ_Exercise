2f8d724599709257b2bc0ec20c6cdb2f03b104ea
jame2408 <jame2408@gmail.com>
2017-11-04 17:27:38 +0800

    Zip 結合 new 之實務用法

---
+++ 12_Zip/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Zip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Zip();
+        }
+
+        static void Zip()
+        {
+            int[] data1 = { 1, 3, 5, 7, 9 };
+            int[] data2 = { 3, 7 };
+            foreach (var item in data1.Zip(data2, (x, y) => new {
+                Name = x, // x: data1 元素，並給予可識別名稱
+                Age = y // y: data2 元素，並給予可識別名稱
+            }))
+            {
+                Console.WriteLine(item);
+                // Console.WriteLine($"{item.Name}: {item.Age}");
+            }
+        }
+    }
+}
