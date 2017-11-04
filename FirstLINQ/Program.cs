91e8bf09db1de50dee5df41ab69536c45588fe6c
jame2408 <jame2408@gmail.com>
2017-11-04 10:21:16 +0800

    初探 LINQ To Object

---
+++ FirstLINQ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = { 1, 3, 5, 7, 9};
+            foreach (var item in Filter(data, (s) => s > 5))
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        private static List<T> Filter<T>(T[] source, Func<T, bool> func)
+        {
+            return (from item in source where func(item) select item).ToList();
+        }
+    }
+}
