d35de6de56a1bfc1c7e9947b21cd25edb21660ba
jame2408 <jame2408@gmail.com>
2017-11-04 10:36:12 +0800

    將 LINQ 運用於 .net framework function 中。（撈取 C:\\Windows 下所有副檔名為 .exe 檔案名稱）

---
+++ 3_LINQInDotNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_LINQInDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = from s1 in Directory.GetFiles(@"C:\\Windows")
+                         where s1.EndsWith(".exe")
+                         select s1;
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
