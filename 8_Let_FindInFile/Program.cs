5073d79a7f0eb882813a4615e458ecdadaf9cd3e
jame2408 <jame2408@gmail.com>
2017-11-04 14:07:23 +0800

    使用 let 實做 Find In Files（檔案內容搜尋）功能

---
+++ 8_Let_FindInFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Let_FindInFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result =
+                from s in Directory.GetFiles(@"C:\Windows", "*.ini")
+                let content = File.ReadAllText(s)
+                where content.Contains("drivers")
+                select s;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
