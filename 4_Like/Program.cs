a73becb0509f83a4e45a1986e5404567736c5c38
jame2408 <jame2408@gmail.com>
2017-11-04 11:16:20 +0800

    LINQ 中做到類似 SQL 語法中的 Like。

---
+++ 4_Like/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Like
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = {"aaa", "bbb", "abc"};
+            var result = data.Where(s => s.Contains("b"));
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
