8bd08a28cb01bb8c20b90e8ce0e6e361fcedd482
jame2408 <jame2408@gmail.com>
2017-11-11 13:18:14 +0800

    於 google 圖片中輸入關鍵字，使用 LINQ 取出所有<img tag 中 的 src 內容。

---
+++ 18_GetImageUrls/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_GetImageUrls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetImageUrls("jolin");
+            Console.ReadLine();
+        }
+
+        static async void GetImageUrls(string query)
+        {
+            HttpClient client = new HttpClient();
+            //var result = new List<string>();
+            var content = await client.GetStringAsync($"http://images.google.com/search?tbm=isch&q={query}");
+
+            // use linq to parse html then get all <img src content
+            var result = content.Split('>').Where(a => a.StartsWith("<img ")).Select(s => s.Split(' ').Where(x => x.StartsWith("src")));
+
+            foreach (var item in result)
+            {
+                foreach (var subitem in item)
+                {
+                    Console.WriteLine(subitem);
+                }
+            }
+        }
+    }
+}
