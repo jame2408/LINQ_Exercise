52ad9f4e366e5278273a7b8d982a49341ff58271
jame2408 <jame2408@gmail.com>
2017-11-11 10:35:26 +0800

    利用 Repeat 產生出指定數量的物件

---
+++ 15_Repeat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_Repeat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RepeatDemoVer2();
+        }
+
+        static void RepeatDemo()
+        {
+            var result = Enumerable.Repeat(new Person(), 10);
+            foreach (var item in result)
+                Console.WriteLine(item.ID);
+        }
+
+        static void RepeatDemoVer2()
+        {
+            Random r = new Random();
+            var result = Enumerable.Repeat(new Person(), 10).Select(a =>
+                new Person() { ID = r.Next(), Name = Guid.NewGuid().ToString() });
+            foreach (var item in result)
+                Console.WriteLine($"ID : {item.ID}, Name : {item.Name}");
+        }
+    }
+
+    internal class Person
+    {
+        public int ID;
+        public string Name { get; set; }
+    }
+}
