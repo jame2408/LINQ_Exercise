5a414ea3944a91df4e2d7ceec7b706feba02fbc7
jame2408 <jame2408@gmail.com>
2017-11-11 10:47:54 +0800

    運用 Select 來平展物件

---
+++ 16_FlatObject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_FlatObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new Person() { Name = "James", Age = 18, City = "Taipei" };
+
+            // 運用 Select 來平展物件
+            var result = persons.GetType().GetProperties().Select(
+                a => new
+                {
+                    Name = a.Name,
+                    Value = a.GetValue(persons)
+                });
+
+            foreach (var item in result)
+                Console.WriteLine($"{item.Name} : {item.Value}");
+        }
+    }
+
+    class Person
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+}
