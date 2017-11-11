1d44042d4ef0cf11bc3fcf9e25ea061bcdefa9fc
jame2408 <jame2408@gmail.com>
2017-11-11 11:54:45 +0800

    集合批次處理，想像你有 100 個 Person 物件在一個集合中，你每次只想處理 4 筆。

--- 17_RepeatRandomData/Program.cs
+++ 17_RepeatRandomData/Program.cs
@@ -19,6 +19,7 @@ namespace _17_RepeatRandomData
                 "Tokyo"
             };
 
+            // 產生亂數資料
             var random = new Random();
             var result = Enumerable.Repeat<Person>(Person.Empty, 100).Select(
                 s =>
@@ -32,11 +33,28 @@ namespace _17_RepeatRandomData
                         City = citys[random.Next(citys.Length)]
                     };
                 }).ToList();
-
+            // 顯示產生的亂數資料
             foreach (var item in result)
             {
                 Console.WriteLine($"Name: {item.Name}, Age: {item.Age}, City: {item.City}");
             }
+            Console.ReadLine();
+
+            // 將亂數資料改成 4 筆一組分組，批次執行
+            var batch = 4;
+            var list = result.Select((item, index) => new {item, index})
+                .GroupBy(x => x.index / batch)
+                .Select(g => g.Select(x => x.item));
+            foreach (var item in list)
+            {
+                foreach (var subitem in item)
+                {
+                    Console.WriteLine($"Name: {subitem.Name}");
+                }
+                Console.WriteLine("------------------------------");
+            }
+            Console.ReadLine();
+
         }
     }
 
